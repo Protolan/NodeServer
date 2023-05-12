71853cfeb39fd6f868589e8fa93eeeb158a8f7e8
Protolan <alan.gumerov@yandex.ru>
2023-05-12 13:13:45 +0300

    folder update

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const path = require("path");
 
 const app = express();
 const port = 80;
+const root = "/var/www";
 
-app.use(express.static("/var/wwww"));
+app.use(express.static(root));
 
 app.get("/", (req, res) => {
-  res.sendFile("/var/www/index.html");
+  const folderPath = req.query.folder;
+
+  if (folderPath) {
+    const indexPath = path.join(root, folderPath, "index.html");
+    res.sendFile(indexPath);
+  } else {
+    res.status(400).send("Путь к папке не указан");
+  }
 });
 
 app.listen(port, () => {
