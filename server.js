cb4340feca09838148b352072e559fc8ce6ab892
Protolan <alan.gumerov@yandex.ru>
2023-05-12 12:43:35 +0300

    fix path

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static("/var/html"));
+app.use(express.static("/var/wwww"));
 
 app.get("/", (req, res) => {
-  res.sendFile("/var/html/index.html");
+  res.sendFile("/var/www/index.html");
 });
 
 app.listen(port, () => {
